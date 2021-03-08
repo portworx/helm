4e95451407a49bb0a257e1e343ff0a5699ee8f26
diptianjan <dipti@portworx.com>
2021-03-08 22:01:27 +0530

    Addessed the review comments.

--- single_chart_migration/migration.sh
+++ single_chart_migration/migration.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Script usage examples
+# 
+# Ex: ./migration.sh --namespace px-backup --helmrepo portworx
+# When using the portworx/helm git repo directly for airgapped environments 
+# Ex: ./migration.sh --namespace px-backup --helmrepo /root/portworx/helm
+
 saListMonitor="px-monitor pxcentral-prometheus pxcentral-prometheus-operator"
 secretListMonitor="pxcentral-cortex pxcentral-cortex-cassandra"
 cmListMonitor="pxcentral-cortex-nginx pxcentral-grafana-dashboard-config pxcentral-grafana-dashboards pxcentral-grafana-ini-config pxcentral-grafana-source-config pxcentral-monitor-configmap"
@@ -47,7 +53,9 @@ usage()
    echo "Usage: $0 --namespace <namespace> --helmrepo <helm repo name> --kubeconfig <kubeconfig file name> "
    echo -e "\t--namespace <namespace> namespace in which px-central charts are installed"
    echo -e "\t--helmrepo <helm repo name for px-central components> helm repo name , can get with helm repo list command"
-   echo -e "\t--kubeconfig <kubeconfig file path> kubeconfig file to set the context, Optional"
+   echo -e "\n\t Optional parameters"
+   echo -e "\t\t--kubeconfig <kubeconfig file path> kubeconfig file to set the context"
+
    exit 1 # Exit script after printing help
 }
 
@@ -68,28 +76,38 @@ change_annotation() {
     resourcetype=$1
     resource=$2
 
-    cmd="$kubectl_cmd -n $namespace annotate $resourcetype $resource "meta.helm.sh/release-name=$pxbackup_release" --overwrite"
-    echo -e $cmd
-    $cmd
-    if [ $? -ne 0 ]; then
-        echo -e "Failed: $cmd, exiting from the update script"
-        exit 1
+    does_exist_cmd="$kubectl_cmd -n $namespace get $resourcetype $resource"
+    echo -e $does_exist_cmd
+    $does_exist_cmd
+    if [ $? -eq 0 ]; then
+        cmd="$kubectl_cmd -n $namespace annotate $resourcetype $resource "meta.helm.sh/release-name=$pxbackup_release" --overwrite"
+        echo -e $cmd
+        $cmd
+        if [ $? -ne 0 ]; then
+            echo -e "Failed: $cmd, exiting from the update script"
+            exit 1
+        fi
+        echo -e "Success: $cmd"
     fi
-    echo -e "Success: $cmd"
 }
 
 delete_resource() {
     resourcetype=$1
     resource=$2
 
-    cmd="$kubectl_cmd -n $namespace delete $resourcetype $resource"
-    echo -e $cmd
-    $cmd
-    if [ $? -ne 0 ]; then
-        echo -e "Faile: $cmd, exiting from the update script"
-        exit 1
+    does_exist_cmd="$kubectl_cmd -n $namespace get $resourcetype $resource"
+    echo -e $does_exist_cmd
+    $does_exist_cmd
+    if [ $? -eq 0 ]; then
+        cmd="$kubectl_cmd -n $namespace delete $resourcetype $resource"
+        echo -e $cmd
+        $cmd
+        if [ $? -ne 0 ]; then
+            echo -e "Failed: $cmd, exiting from the update script"
+            exit 1
+        fi
+        echo -e "Success: $cmd"
     fi
-    echo -e "Success: $cmd"
 }
 
 set_enabled_modules() {
@@ -165,7 +183,14 @@ do
     esac
 done
 
-if [ "$namespace" == "" ] || [ "$helmrepo" == "" ]; then
+if [ "$namespace" == "" ]; then
+    echo "--namespace is empty"
+    usage
+    exit 1
+fi
+
+if [ "$helmrepo" == "" ]; then
+    echo "--helmrepo is empty"
     usage
     exit 1
 fi
@@ -175,28 +200,29 @@ if [ "$kubeconfig" != "" ]; then
     helm_cmd="helm --kubeconfig $kubeconfig"
 fi
 
-echo "Step-1 : Finding out the enabled modules"
+echo -e "\nStep-1 : Looking for enabled features"
 set_enabled_modules $namespace
 echo "pxbackup: $pxbackup_enabled, pxmonitor: $pxmonitor_enabled, pxlicenseserver: $pxls_enabled"
 
-echo "Step-2 : Finding the installed releases for the components"
+echo -e "\nStep-2 : Searching the installed helm releases for the components"
 find_releases $namespace
 echo "pxbackup release: $pxbackup_release, pxmonitor release: $pxmonitor_release, pxlicenseserver release: $pxls_release"
+verify_release $namespace $pxbackup_release
 
 # Get all values in a single file
-echo "Step-3 : Putting the values in helm_values.yaml file"
+echo -e "\nStep-3 : Generating helm_values.yaml"
 $helm_cmd get values $pxbackup_release --namespace $namespace -o yaml > helm_values.yaml
 if [ "$pxmonitor_enabled" == true ]; then
     $helm_cmd get values $pxmonitor_release --namespace $namespace -o yaml >> helm_values.yaml
 fi
-if [ "$pxls_enabled" != "" ]; then
+if [ "$pxls_enabled" == true ]; then
     $helm_cmd get values $pxls_release --namespace $namespace -o yaml >> helm_values.yaml
 fi
 
 # Change the annotations
-echo "Step-4 : Changing annotations of px-monitor and px-license-server componets if they have been installed"
-if [ "$pxmonitor_enabled" != "" ]; then
-    echo "Changing the annotations of existing px-monitor componenets"
+echo -e "\nStep-4 : Changing annotations of px-monitor and px-license-server components"
+if [ "$pxmonitor_enabled" == true ]; then
+    echo "Changing the annotations of existing px-monitor components"
     change_annotation "serviceaccount" "$saListMonitor"
     change_annotation "secret" "$secretListMonitor"
     change_annotation "configmap" "$cmListMonitor"
@@ -215,8 +241,8 @@ if [ "$pxmonitor_enabled" != "" ]; then
     delete_resource "deployment" "$deploymentListMonitorDelete"
 fi
 
-if [ "$pxls_enabled" != "" ]; then
-    echo "Changing the annotations of existing px-license-server componenets"
+if [ "$pxls_enabled" == true ]; then
+    echo "Changing the annotations of existing px-license-server components"
     change_annotation "secret" "$secretListLS"
     change_annotation "configmap" "$cmListLS"
     change_annotation "service" "$svcListLS"
@@ -225,13 +251,13 @@ if [ "$pxls_enabled" != "" ]; then
 fi
 
 # Delete the post-install job
-echo "Step-5 : Deleting post-install hook job before upgrade"
+echo -e "\nStep-5 : Deleting post-install hook job before upgrade"
 delete_job_cmd="$kubectl_cmd --namespace $namespace delete job $post_install_job"
 echo $delete_job_cmd
 $delete_job_cmd
 
 # Do the upgrade
-echo "Step-6 : Going to do upgrade now"
+echo -e "\nStep-6 : Starting upgrade now"
 # Version as global
 upgrade_cmd="$helm_cmd --namespace $namespace upgrade $pxbackup_release $helmrepo/px-central --version $px_central_version -f helm_values.yaml"
 
