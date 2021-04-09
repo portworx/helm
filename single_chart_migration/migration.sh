257705bc03f61978f37144fe388597dc48677e1a
diptiranjanpx <51692470+diptiranjanpx@users.noreply.github.com>
2021-04-09 17:56:38 +0530

    [PB-1249]: Disabling etcd from 1.3.0 onwards by default so that it does not get deployed. (#242)
    Only for mongomigration, it will be enabled during upgrade in migration script.
    Signed-off by:  Diptiranjan

--- single_chart_migration/migration.sh
+++ single_chart_migration/migration.sh
@@ -263,6 +263,9 @@ upgrade_cmd="$helm_cmd --namespace $namespace upgrade $pxbackup_release $helmrep
 
 if [ "$pxbackup_enabled" == true ]; then
     upgrade_cmd="$upgrade_cmd --set pxbackup.enabled=true"
+    # mongomigration will be set to incomplete by default, since this script will be called for upgrade only
+    # Also etcd statefulset needs to be retained.
+    upgrade_cmd="$upgrade_cmd --set pxbackup.mongoMigration=incomplete,pxbackup.datastore=mongodb,pxbackup.retainEtcd=true"
 fi
 if [ "$pxmonitor_enabled" == true ]; then
     upgrade_cmd="$upgrade_cmd --set pxmonitor.enabled=true"
@@ -270,11 +273,6 @@ fi
 if [ "$pxls_enabled" == true ]; then
     upgrade_cmd="$upgrade_cmd --set pxlicenseserver.enabled=true"
 fi
-
-# TODO: for now adding it default. Need to add based on the version check.
-upgrade_cmd="$upgrade_cmd --set pxbackup.datastore=mongodb"
-# mongomigration will be set to incomplete by default, since this script will be called for upgrade only
-upgrade_cmd="$upgrade_cmd --set pxbackup.mongomigration=incomplete"
 
 echo "upgrade command: $upgrade_cmd"
 $upgrade_cmd
