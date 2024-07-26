0c31ed647379b783a0c0a97ae63e87b97675c00e
hitesh-wani-px <hwani+github@purestorage.com>
2024-07-26 15:48:52 +0530

    PWX-37686 |  Added nodes specific configuration  (#603)
    * Added nodes specific configuration
    
    Signed-off-by: hitesh-wani-px <hwani+github@purestorage.com>
    
    What this PR does / why we need it:
    Added nodes specific configuration in storagecluster template.
    
    Which issue(s) this PR fixes (optional)
    Closes #https://purestorage.atlassian.net/browse/PWX-37686
    
    ---------
    
    Signed-off-by: hitesh-wani-px <hwani+github@purestorage.com>

--- test/portworx/storagecluster_helm_template_test.go
+++ test/portworx/storagecluster_helm_template_test.go
@@ -126,6 +126,13 @@ func TestStorageClusterHelmTemplate(t *testing.T) {
 				ValuesFiles: []string{"./testValues/storagecluster_placement.yaml"},
 			},
 		},
+		{
+			name:           "TestNodesConfiguration",
+			resultFileName: "storagecluster_nodes_configuration.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_nodes_configuration.yaml"},
+			},
+		},
 	}
 
 	for _, testCase := range testCases {
