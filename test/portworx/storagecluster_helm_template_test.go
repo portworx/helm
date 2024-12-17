9ece38f7bae5a6feaf74d19f5ce99b57245a4bfd
hitesh-wani-px <hwani+github@purestorage.com>
2024-12-17 16:01:44 +0530

    update 4.1.0 tar

--- test/portworx/storagecluster_helm_template_test.go
+++ test/portworx/storagecluster_helm_template_test.go
@@ -25,156 +25,156 @@ func TestStorageClusterHelmTemplate(t *testing.T) {
 		resultFileName   string
 		expectedErrorMsg string
 	}{
-		// {
-		// 	name:             "Failed: NoEtcdConfigurationProvided",
-		// 	expectedErrorMsg: "A valid ETCD url in the format etcd:http://<your-etcd-endpoint> is required.",
-		// 	helmOption: &helm.Options{SetValues: map[string]string{
-		// 		"internalKVDB": "false",
-		// 	}},
-		// },
-		// {
-		// 	name:           "TestAllComponentsEnabled",
-		// 	resultFileName: "storagecluster_all_compenents_enabled.yaml",
-		// 	helmOption: &helm.Options{
-		// 		ValuesFiles: []string{"./testValues/storagecluster_all_components_enabled.yaml"},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestDefaultChartValues",
-		// 	resultFileName: "storagecluster_with_default_values.yaml",
-		// 	helmOption: &helm.Options{
-		// 		SetValues: map[string]string{"internalKVDB": "true"},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestCustomRegistry",
-		// 	resultFileName: "storagecluster_custom_registry.yaml",
-		// 	helmOption: &helm.Options{
-		// 		ValuesFiles: []string{"./testValues/storagecluster_custom_registry.yaml"},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestExternalETCD",
-		// 	resultFileName: "storagecluster_external_etcd.yaml",
-		// 	helmOption: &helm.Options{
-		// 		ValuesFiles: []string{"./testValues/storagecluster_external_etcd.yaml"},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestCSITopologyEnabled",
-		// 	resultFileName: "storagecluster_csi_topology_enabled.yaml",
-		// 	helmOption: &helm.Options{
-		// 		SetValues: map[string]string{
-		// 			"csi.enabled":          "true",
-		// 			"csi.topology.enabled": "true",
-		// 		},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestCSISnapshotControllerEnabled",
-		// 	resultFileName: "storagecluster_csi_Snapshot_Controller_enabled.yaml",
-		// 	helmOption: &helm.Options{
-		// 		SetValues: map[string]string{
-		// 			"csi.enabled":                   "true",
-		// 			"csi.installSnapshotController": "true",
-		// 		},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestCSIDisabled",
-		// 	resultFileName: "storagecluster_csi_disabled.yaml",
-		// 	helmOption: &helm.Options{
-		// 		SetValues: map[string]string{
-		// 			"csi.enabled":                   "false",
-		// 			"csi.installSnapshotController": "true",
-		// 			"csi.topology.enabled":          "true",
-		// 		},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestMonitoringWithAllValues",
-		// 	resultFileName: "storagecluster_monitoring.yaml",
-		// 	helmOption: &helm.Options{
-		// 		ValuesFiles: []string{"./testValues/storagecluster_monitoring.yaml"},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestMonitoringWithExportMatrixEnabled",
-		// 	resultFileName: "storagecluster_monitoring_enabled_exportmatrix.yaml",
-		// 	helmOption: &helm.Options{
-		// 		SetValues: map[string]string{
-		// 			"monitoring.prometheus.enabled":       "false",
-		// 			"monitoring.prometheus.exportMetrics": "true",
-		// 			"monitoring.telemetry":                "false",
-		// 		},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestMonitoringConditionByDisablingAllComponents",
-		// 	resultFileName: "storagecluster_monitoring_disable_by_all_components_disabled.yaml",
-		// 	helmOption: &helm.Options{
-		// 		SetValues: map[string]string{
-		// 			"monitoring.prometheus.enabled":       "false",
-		// 			"monitoring.prometheus.exportMetrics": "false",
-		// 			"monitoring.telemetry":                "false",
-		// 		},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestStork",
-		// 	resultFileName: "storagecluster_stork.yaml",
-		// 	helmOption: &helm.Options{
-		// 		ValuesFiles: []string{"./testValues/storagecluster_stork.yaml"},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestVolumes",
-		// 	resultFileName: "storagecluster_volumes.yaml",
-		// 	helmOption: &helm.Options{
-		// 		ValuesFiles: []string{"./testValues/storagecluster_volumes.yaml"},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestPlacementNodeAffinity",
-		// 	resultFileName: "storagecluster_nodeAffinity.yaml",
-		// 	helmOption: &helm.Options{
-		// 		ValuesFiles: []string{"./testValues/storagecluster_nodeAffinity.yaml"},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestPlacementTolerations",
-		// 	resultFileName: "storagecluster_placement.yaml",
-		// 	helmOption: &helm.Options{
-		// 		ValuesFiles: []string{"./testValues/storagecluster_placement.yaml"},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestRuntimeOptions",
-		// 	resultFileName: "storagecluster_runtimeOptions.yaml",
-		// 	helmOption: &helm.Options{
-		// 		SetValues: map[string]string{
-		// 			"runtimeOptions": "num_io_threads=10",
-		// 			"internalKVDB":   "true",
-		// 		},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestFeatureGates",
-		// 	resultFileName: "storagecluster_featureGates.yaml",
-		// 	helmOption: &helm.Options{
-		// 		SetValues: map[string]string{
-		// 			"featureGates": "CSI=true",
-		// 			"internalKVDB": "true",
-		// 		},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestUpdateStrategyRollingUpdate",
-		// 	resultFileName: "storagecluster_updatestrategy_rollingupdate.yaml",
-		// 	helmOption: &helm.Options{
-		// 		ValuesFiles: []string{"./testValues/storagecluster_updatestrategy_rollingupdate.yaml"},
-		// 	},
-		// },
+		{
+			name:             "Failed: NoEtcdConfigurationProvided",
+			expectedErrorMsg: "A valid ETCD url in the format etcd:http://<your-etcd-endpoint> is required.",
+			helmOption: &helm.Options{SetValues: map[string]string{
+				"internalKVDB": "false",
+			}},
+		},
+		{
+			name:           "TestAllComponentsEnabled",
+			resultFileName: "storagecluster_all_compenents_enabled.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_all_components_enabled.yaml"},
+			},
+		},
+		{
+			name:           "TestDefaultChartValues",
+			resultFileName: "storagecluster_with_default_values.yaml",
+			helmOption: &helm.Options{
+				SetValues: map[string]string{"internalKVDB": "true"},
+			},
+		},
+		{
+			name:           "TestCustomRegistry",
+			resultFileName: "storagecluster_custom_registry.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_custom_registry.yaml"},
+			},
+		},
+		{
+			name:           "TestExternalETCD",
+			resultFileName: "storagecluster_external_etcd.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_external_etcd.yaml"},
+			},
+		},
+		{
+			name:           "TestCSITopologyEnabled",
+			resultFileName: "storagecluster_csi_topology_enabled.yaml",
+			helmOption: &helm.Options{
+				SetValues: map[string]string{
+					"csi.enabled":          "true",
+					"csi.topology.enabled": "true",
+				},
+			},
+		},
+		{
+			name:           "TestCSISnapshotControllerEnabled",
+			resultFileName: "storagecluster_csi_Snapshot_Controller_enabled.yaml",
+			helmOption: &helm.Options{
+				SetValues: map[string]string{
+					"csi.enabled":                   "true",
+					"csi.installSnapshotController": "true",
+				},
+			},
+		},
+		{
+			name:           "TestCSIDisabled",
+			resultFileName: "storagecluster_csi_disabled.yaml",
+			helmOption: &helm.Options{
+				SetValues: map[string]string{
+					"csi.enabled":                   "false",
+					"csi.installSnapshotController": "true",
+					"csi.topology.enabled":          "true",
+				},
+			},
+		},
+		{
+			name:           "TestMonitoringWithAllValues",
+			resultFileName: "storagecluster_monitoring.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_monitoring.yaml"},
+			},
+		},
+		{
+			name:           "TestMonitoringWithExportMatrixEnabled",
+			resultFileName: "storagecluster_monitoring_enabled_exportmatrix.yaml",
+			helmOption: &helm.Options{
+				SetValues: map[string]string{
+					"monitoring.prometheus.enabled":       "false",
+					"monitoring.prometheus.exportMetrics": "true",
+					"monitoring.telemetry":                "false",
+				},
+			},
+		},
+		{
+			name:           "TestMonitoringConditionByDisablingAllComponents",
+			resultFileName: "storagecluster_monitoring_disable_by_all_components_disabled.yaml",
+			helmOption: &helm.Options{
+				SetValues: map[string]string{
+					"monitoring.prometheus.enabled":       "false",
+					"monitoring.prometheus.exportMetrics": "false",
+					"monitoring.telemetry":                "false",
+				},
+			},
+		},
+		{
+			name:           "TestStork",
+			resultFileName: "storagecluster_stork.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_stork.yaml"},
+			},
+		},
+		{
+			name:           "TestVolumes",
+			resultFileName: "storagecluster_volumes.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_volumes.yaml"},
+			},
+		},
+		{
+			name:           "TestPlacementNodeAffinity",
+			resultFileName: "storagecluster_nodeAffinity.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_nodeAffinity.yaml"},
+			},
+		},
+		{
+			name:           "TestPlacementTolerations",
+			resultFileName: "storagecluster_placement.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_placement.yaml"},
+			},
+		},
+		{
+			name:           "TestRuntimeOptions",
+			resultFileName: "storagecluster_runtimeOptions.yaml",
+			helmOption: &helm.Options{
+				SetValues: map[string]string{
+					"runtimeOptions": "num_io_threads=10",
+					"internalKVDB":   "true",
+				},
+			},
+		},
+		{
+			name:           "TestFeatureGates",
+			resultFileName: "storagecluster_featureGates.yaml",
+			helmOption: &helm.Options{
+				SetValues: map[string]string{
+					"featureGates": "CSI=true",
+					"internalKVDB": "true",
+				},
+			},
+		},
+		{
+			name:           "TestUpdateStrategyRollingUpdate",
+			resultFileName: "storagecluster_updatestrategy_rollingupdate.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_updatestrategy_rollingupdate.yaml"},
+			},
+		},
 		{
 			name:           "TestUpdateStrategyRollingSmartUpdate",
 			resultFileName: "storagecluster_updatestrategy_rollingupdate_smart.yaml",
@@ -182,120 +182,120 @@ func TestStorageClusterHelmTemplate(t *testing.T) {
 				ValuesFiles: []string{"./testValues/storagecluster_updatestrategy_rollingupdate_smart.yaml"},
 			},
 		},
-		// {
-		// 	name:           "TestUpdateStrategyOndelete",
-		// 	resultFileName: "storagecluster_updatestrategy_ondelete.yaml",
-		// 	helmOption: &helm.Options{
-		// 		SetValues: map[string]string{
-		// 			"updateStrategy.type":                 "OnDelete",
-		// 			"updateStrategy.autoUpdateComponents": "Once",
-		// 		},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestUpdateStrategyWithInvalidType",
-		// 	resultFileName: "storagecluster_updatestrategy_invalid_type.yaml",
-		// 	helmOption: &helm.Options{
-		// 		SetValues: map[string]string{
-		// 			"updateStrategy.type":                 "Invalid",
-		// 			"updateStrategy.autoUpdateComponents": "None",
-		// 		},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestSecurityEnabled",
-		// 	resultFileName: "storagecluster_security_enabled.yaml",
-		// 	helmOption: &helm.Options{
-		// 		ValuesFiles: []string{"./testValues/storagecluster_security_enabled.yaml"},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestSecurityDisabled",
-		// 	resultFileName: "storagecluster_with_default_values.yaml",
-		// 	helmOption: &helm.Options{
-		// 		ValuesFiles: []string{"./testValues/storagecluster_security_disabled.yaml"},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestPortworxContainerResources",
-		// 	resultFileName: "storagecluster_portworx_container_resources.yaml",
-		// 	helmOption: &helm.Options{
-		// 		ValuesFiles: []string{"./testValues/storagecluster_portworx_container_resources.yaml"},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestCustomMetadata",
-		// 	resultFileName: "storagecluster_custom_metadata.yaml",
-		// 	helmOption: &helm.Options{
-		// 		ValuesFiles: []string{"./testValues/storagecluster_custom_metadata.yaml"},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestNodesConfiguration",
-		// 	resultFileName: "storagecluster_nodes_configuration.yaml",
-		// 	helmOption: &helm.Options{
-		// 		ValuesFiles: []string{"./testValues/storagecluster_nodes_configuration.yaml"},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestAutoPilot",
-		// 	resultFileName: "storagecluster_autopilot.yaml",
-		// 	helmOption: &helm.Options{
-		// 		ValuesFiles: []string{"./testValues/storagecluster_autopilot.yaml"},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestCloudStorage",
-		// 	resultFileName: "storagecluster_cloudstorage.yaml",
-		// 	helmOption: &helm.Options{
-		// 		ValuesFiles: []string{"./testValues/storagecluster_cloudstorage.yaml"},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestCloudStorageAKS",
-		// 	resultFileName: "storagecluster_cloudstorage_aks.yaml",
-		// 	helmOption: &helm.Options{
-		// 		ValuesFiles: []string{"./testValues/storagecluster_cloudstorage_aks.yaml"},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestStorageSpecDevices",
-		// 	resultFileName: "storagecluster_storage_devices.yaml",
-		// 	helmOption: &helm.Options{
-		// 		ValuesFiles: []string{"./testValues/storagecluster_storage_devices.yaml"},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestStorageSpecWithUseAll",
-		// 	resultFileName: "storagecluster_storage_use_all.yaml",
-		// 	helmOption: &helm.Options{
-		// 		ValuesFiles: []string{"./testValues/storagecluster_storage_use_all.yaml"},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestStorageSpecWithUseAll",
-		// 	resultFileName: "storagecluster_storage_use_all.yaml",
-		// 	helmOption: &helm.Options{
-		// 		ValuesFiles: []string{"./testValues/storagecluster_storage_use_partitions.yaml"},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestStorageSpecWithUsePartitions",
-		// 	resultFileName: "storagecluster_storage_use_partitions.yaml",
-		// 	helmOption: &helm.Options{
-		// 		ValuesFiles: []string{"./testValues/storagecluster_storage_use_partitions.yaml"},
-		// 	},
-		// },
-		// {
-		// 	name:           "TestNonDisruptiveUpgradeAndHealthChecksAnnotation",
-		// 	resultFileName: "storagecluster_non_disruptive_and_health_cheks_hannotation.yaml",
-		// 	helmOption: &helm.Options{
-		// 		SetValues: map[string]string{
-		// 			"nonDisruptivek8sUpgrade": "true",
-		// 			"skipHealthChecks":        "true",
-		// 		},
-		// 	},
-		// },
+		{
+			name:           "TestUpdateStrategyOndelete",
+			resultFileName: "storagecluster_updatestrategy_ondelete.yaml",
+			helmOption: &helm.Options{
+				SetValues: map[string]string{
+					"updateStrategy.type":                 "OnDelete",
+					"updateStrategy.autoUpdateComponents": "Once",
+				},
+			},
+		},
+		{
+			name:           "TestUpdateStrategyWithInvalidType",
+			resultFileName: "storagecluster_updatestrategy_invalid_type.yaml",
+			helmOption: &helm.Options{
+				SetValues: map[string]string{
+					"updateStrategy.type":                 "Invalid",
+					"updateStrategy.autoUpdateComponents": "None",
+				},
+			},
+		},
+		{
+			name:           "TestSecurityEnabled",
+			resultFileName: "storagecluster_security_enabled.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_security_enabled.yaml"},
+			},
+		},
+		{
+			name:           "TestSecurityDisabled",
+			resultFileName: "storagecluster_with_default_values.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_security_disabled.yaml"},
+			},
+		},
+		{
+			name:           "TestPortworxContainerResources",
+			resultFileName: "storagecluster_portworx_container_resources.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_portworx_container_resources.yaml"},
+			},
+		},
+		{
+			name:           "TestCustomMetadata",
+			resultFileName: "storagecluster_custom_metadata.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_custom_metadata.yaml"},
+			},
+		},
+		{
+			name:           "TestNodesConfiguration",
+			resultFileName: "storagecluster_nodes_configuration.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_nodes_configuration.yaml"},
+			},
+		},
+		{
+			name:           "TestAutoPilot",
+			resultFileName: "storagecluster_autopilot.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_autopilot.yaml"},
+			},
+		},
+		{
+			name:           "TestCloudStorage",
+			resultFileName: "storagecluster_cloudstorage.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_cloudstorage.yaml"},
+			},
+		},
+		{
+			name:           "TestCloudStorageAKS",
+			resultFileName: "storagecluster_cloudstorage_aks.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_cloudstorage_aks.yaml"},
+			},
+		},
+		{
+			name:           "TestStorageSpecDevices",
+			resultFileName: "storagecluster_storage_devices.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_storage_devices.yaml"},
+			},
+		},
+		{
+			name:           "TestStorageSpecWithUseAll",
+			resultFileName: "storagecluster_storage_use_all.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_storage_use_all.yaml"},
+			},
+		},
+		{
+			name:           "TestStorageSpecWithUseAll",
+			resultFileName: "storagecluster_storage_use_all.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_storage_use_partitions.yaml"},
+			},
+		},
+		{
+			name:           "TestStorageSpecWithUsePartitions",
+			resultFileName: "storagecluster_storage_use_partitions.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/storagecluster_storage_use_partitions.yaml"},
+			},
+		},
+		{
+			name:           "TestNonDisruptiveUpgradeAndHealthChecksAnnotation",
+			resultFileName: "storagecluster_non_disruptive_and_health_cheks_hannotation.yaml",
+			helmOption: &helm.Options{
+				SetValues: map[string]string{
+					"nonDisruptivek8sUpgrade": "true",
+					"skipHealthChecks":        "true",
+				},
+			},
+		},
 	}
 
 	for _, testCase := range testCases {
