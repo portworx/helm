53381f9fd4c98259e386859ecb1d8e9070f7afa2
Swarupa Vijaykumar <svijaykumar@purestorage.com>
2025-07-25 10:11:22 +0000

    helm changes to support instance volumes

--- test/portworx/storagecluster_helm_template_test.go
+++ test/portworx/storagecluster_helm_template_test.go
@@ -367,6 +367,15 @@ func TestStorageClusterHelmTemplate(t *testing.T) {
 				},
 			},
 		},
+		{
+			name:           "TestInstanceVolumeK8sUpgrade",
+			resultFileName: "storagecluster_instance_volume_k8s_upgrade.yaml",
+			helmOption: &helm.Options{
+				SetValues: map[string]string{
+					"instanceVolumeK8sUpgrade": "true",
+				},
+			},
+		},
 	}
 
 	for _, testCase := range testCases {
