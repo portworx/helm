3ad21fa54130aa234946e4649006f9dc6197cda8
hitesh-wani-px <hwani+github@purestorage.com>
2025-09-03 20:15:44 +0530

    PWX-46555 | Helm release for Operator 25.3.0 (#800)
    * helm changes to support instance volumes
    
    * making suggested changes
    
    * Helm changes for DRW-phase-2 (#789)
    
    * Add helm changes for workload identity feature
    
    * Helm release for OP-25.3.0
    
    * Update CRDs and update containerSelector field to containerNames
    
    * Fix Uts
    
    * Update Readme
    
    * Revert "Merge pull request #782 from portworx/PWX-45412" (#803)
    
    This reverts commit 9b842b077a63554b0192de65e2a2a3faccb44c11, reversing
    changes made to 0484cd849777bbb51cda6836c4e4c1aa612875b7.
    
    * Update tar
    
    ---------
    
    Co-authored-by: Swarupa Vijaykumar <svijaykumar@purestorage.com>
    Co-authored-by: Swarupa Vijaykumar <123443458+svijaykumar-px@users.noreply.github.com>
    Co-authored-by: Hemakshi Sachdev <hsachdev@purestorage.com>
    Co-authored-by: Hemakshi Sachdev <142894373+hsachdev-px@users.noreply.github.com>

---
+++ test/portworx/componentk8sconfig_helm_template_test.go
@@ -0,0 +1,48 @@
+package portworx_test
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/helm"
+	test_utils "github.com/portworx/helm/test/utils"
+	"github.com/stretchr/testify/require"
+)
+
+func TestComponentK8sConfigHelmTemplate(t *testing.T) {
+
+	t.Parallel()
+
+	// Path to the helm chart we will test
+	helmChartPath, err := filepath.Abs("../../charts/portworx/")
+	// name of template that we want to test
+	templateFileName := "component-k8s-config.yaml"
+	require.NoError(t, err)
+
+	testCases := []struct {
+		name             string
+		helmOption       *helm.Options
+		resultFileName   string
+		expectedErrorMsg string
+	}{
+		{
+			name:           "componentK8sConfig",
+			resultFileName: "componentk8sconfigs.yaml",
+			helmOption: &helm.Options{
+				ValuesFiles: []string{"./testValues/componentk8sconfigs.yaml"},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+
+		testCase := testCase
+
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+			resultFilePath, err := filepath.Abs(filepath.Join("testspec/", testCase.resultFileName))
+			require.NoError(t, err)
+			test_utils.TestRenderedHelmTemplate(t, testCase.helmOption, helmChartPath, templateFileName, resultFilePath, testCase.expectedErrorMsg)
+		})
+	}
+}
